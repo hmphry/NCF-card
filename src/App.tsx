55c8790254de87f73d393e4e54bfa2f5763a81c2
Emily Humphrey <emily@hmphry.com>
2023-02-08 01:27:44 -0500

    small updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className="bg-black min-h-full font-sans">
       <div className='bg-gradient-to-r from-blue to-pink h-2 mb-16'></div>
-      <section className='px-4'>
+      <section className='px-4 max-w-lg mx-auto'>
         <h1 className='text-white text-2xl font-semibold'>Emily Humphrey</h1>
         <p className='text-white text-sm'>Front End Engineer</p>
       </section>
-      <section className='px-4 mt-4'>
+      <section className='px-4 max-w-lg mx-auto mt-4'>
         <a href="data/emilyhumphrey.vcf" download className='flex w-full h-16 rounded-full justify-center items-center bg-gradient-to-r from-blue to-pink text-white font-semibold text-center'>Save Contact</a>
         <LinkCard service="Twitter" url="https://www.twitter.com/heyhmphry/" icon={twitter} />
         <LinkCard service="Instagram" url="https://www.instagram.com/heyhmphry/" icon={instagram} />
--- src/LinkCard.tsx
+++ src/LinkCard.tsx
@@ -1,14 +1,19 @@
-
 import arrow from './assets/arrow-right-solid.svg'
 
-function LinkCard({service, url, icon}) {
+interface linkCardProps {
+    service: String;
+    url: String;
+    icon: unknown;
+}
+
+function LinkCard(props:linkCardProps) {
   return (
-    <a href={url} target="_blank" className='flex items-center justify-between rounded-full mt-6 bg-grey'>
+    <a href={props.url} target="_blank" className='flex items-center justify-between rounded-full mt-6 bg-grey'>
       <div className='flex items-center'>
         <div className='flex items-center justify-center w-16 h-16 rounded-full bg-pink'>
-            <img src={icon} className="w-8 h-8" alt={`Follow Emily on ${service}`} />
+            <img src={props.icon} className="w-8 h-8" alt={`Follow Emily on ${props.service}`} />
         </div>
-        <p className='ml-4 text-white'>{service}</p>
+        <p className='ml-4 text-white'>{props.service}</p>
       </div>
       <img src={arrow} className="w-6 h-6 mr-4" />
     </a>
